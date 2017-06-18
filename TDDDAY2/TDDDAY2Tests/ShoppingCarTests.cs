67fa87d5588ee7edcc705c1299957d634b2852a4
NewmanFeng <charlsepheng@gmail.com>
2017-06-19 00:34:17 +0800

    Second Red

--- TDDDAY2/TDDDAY2Tests/ShoppingCarTests.cs
+++ TDDDAY2/TDDDAY2Tests/ShoppingCarTests.cs
@@ -13,7 +13,7 @@ namespace TDDDAY2.Tests
     public class ShoppingCarTests
     {
         [TestMethod()]
-        public void forAutoGenertateTestTest()
+        public void Buy_One_HollyPoter1_Price_Should_Match()
         {
             var target = new ShoppingCar();
 
@@ -24,6 +24,21 @@ namespace TDDDAY2.Tests
             target.Buy(new Book("HollyPoter5", 0));
 
             var expected = 100m;
+            Assert.AreEqual(expected, target.GetPrice());
+        }
+
+        [TestMethod()]
+        public void Buy_One_HollyPoter1_One_HollyPoter2_Price_Should_Match()
+        {
+            var target = new ShoppingCar();
+
+            target.Buy(new Book("HollyPoter1", 1));
+            target.Buy(new Book("HollyPoter2", 1));
+            target.Buy(new Book("HollyPoter3", 0));
+            target.Buy(new Book("HollyPoter4", 0));
+            target.Buy(new Book("HollyPoter5", 0));
+
+            var expected = 190m;
             Assert.AreEqual(expected, target.GetPrice());
         }
     }
