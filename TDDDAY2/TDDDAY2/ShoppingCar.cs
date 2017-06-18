4bf32930c9b720658ee8bf784d3f07a28b7f90d2
NewmanFeng <charlsepheng@gmail.com>
2017-06-19 00:48:22 +0800

    second green light!

--- TDDDAY2/TDDDAY2/ShoppingCar.cs
+++ TDDDAY2/TDDDAY2/ShoppingCar.cs
@@ -1,20 +1,39 @@
-﻿namespace TDDDAY2
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TDDDAY2
 {
     internal class ShoppingCar
     {
+        private List<Book> _books;
+        protected readonly decimal SinglePrice = 100m;
+
         public ShoppingCar()
         {
+            _books = new List<Book>();
         }
 
-        public void Buy(Book hollyPoter1)
+        public void Buy(Book book)
         {
-
+            _books.Add(book);
 
         }
 
         public decimal GetPrice()
         {
-            return 100m;
+            var totalCount = _books.Sum(book => book.count);
+            var price = 0m;
+
+            if (totalCount == 1)
+            {
+                price = 100m;
+            }
+            else if (totalCount == 2)
+            {
+                price = 100 * totalCount * 0.95m;
+            }
+            return price;
         }
     }
 }
