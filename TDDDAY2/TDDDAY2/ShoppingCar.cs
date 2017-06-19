ef41856936511b9f84d06974fc4ba611f9800a5b
NewmanFeng <charlsepheng@gmail.com>
2017-06-19 10:20:23 +0800

    Refactor 1.100->using SinglePrice 2.Extract get discount method

--- TDDDAY2/TDDDAY2/ShoppingCar.cs
+++ TDDDAY2/TDDDAY2/ShoppingCar.cs
@@ -23,29 +23,55 @@ namespace TDDDAY2
         public decimal GetPrice()
         {
             var totalCount = _books.Sum(book => book.count);
-            var price = 0m;
+            var price = SinglePrice*totalCount*GetDiscount(totalCount);
+
+            //if (totalCount == 1)
+            //{
+            //    price = SinglePrice * totalCount ;
+            //}
+            //else if (totalCount == 2)
+            //{
+            //    price = SinglePrice * totalCount * 0.95m;
+            //}
+            //else if(totalCount==3)
+            //{
+            //    price = SinglePrice*totalCount*0.9m;
+            //}
+            //else if(totalCount==4)
+            //{
+            //    price = SinglePrice * totalCount * 0.8m;
+            //}
+            //else if (totalCount == 5)
+            //{
+            //    price = 100 * totalCount * 0.75m;
+            //}
+            return price;
+        }
+
+        private decimal GetDiscount(int totalCount)
+        {
 
             if (totalCount == 1)
             {
-                price = 100m;
+                return 1;
             }
             else if (totalCount == 2)
             {
-                price = 100 * totalCount * 0.95m;
+            return 0.95m;
             }
-            else if(totalCount==3)
+            else if (totalCount == 3)
             {
-                price = 100*totalCount*0.9m;
+                return 0.9m;
             }
-            else if(totalCount==4)
+            else if (totalCount == 4)
             {
-                price = 100 * totalCount * 0.8m;
+                return 0.8m;
             }
             else if (totalCount == 5)
             {
-                price = 100 * totalCount * 0.75m;
+                return 0.75m;
             }
-            return price;
+            return 1;
         }
     }
 }
